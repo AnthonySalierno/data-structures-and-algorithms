7210954374dcaa9136143324e1c35a9dd62d5f46
Anthony Salierno <anthony.salierno@gmail.com>
2016-08-10 20:02:59 -0700

    Add tests for factorial

--- test/test.js
+++ test/test.js
@@ -1,17 +1,50 @@
 const test = require('tape');
-const i = require('../javascript/algorithms/insertionSort');
-const s = require('../javascript/algorithms/selectionSort');
+const factorial = require('../javascript/algorithms/recursion/factorial');
+const insertion = require('../javascript/algorithms/sorting/insertionSort');
+const selection = require('../javascript/algorithms/sorting/selectionSort');
 
 test('A passing test', (assert) => {
   assert.pass('This test will pass');
   assert.end();
 });
 
+test('Factorial should return null when a negative value is passed in', (assert) => {
+  const actual = factorial.factorial(-1);
+  const expected = null;
+
+  assert.equal(actual, expected, 'Factorial should return null');
+  assert.end();
+});
+
+test('Factorial should return 1 when 0 is passed in', (assert) => {
+  const actual = factorial.factorial(0);
+  const expected = 1;
+
+  assert.equal(actual, expected, 'Factorial should return 1');
+  assert.end();
+});
+
+test('Factorial should return 1 when 1 is passed in', (assert) => {
+  const actual = factorial.factorial(1);
+  const expected = 1;
+
+  assert.equal(actual, expected, 'Factorial should return 1');
+  assert.end();
+});
+
+test('Factorial should return 120 when 5 is passed in', (assert) => {
+  const actual = factorial.factorial(5);
+  const expected = 120;
+
+  assert.equal(actual, expected, 'Factorial should return 120');
+  assert.end();
+});
+
 test('Selection sort should return empty array when an empty array is passed in.', (assert) => {
-  const actualType = Array.isArray(s.selectionSort([]));
+  const actualType = Array.isArray(selection.selectionSort([]));
   const expectedType = true;
 
-  const actualLength = s.selectionSort([]).length;
+  const actualLength = selection.selectionSort([]).length;
   const expectedLength = 0;
 
   assert.equal(actualType, expectedType, 'Selection sort should return an array');
