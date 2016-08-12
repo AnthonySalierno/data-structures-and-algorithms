b86efa263437b55a53f8150f99651a28e9e299a7
Anthony Salierno <anthony.salierno@gmail.com>
2016-08-11 23:59:16 -0700

    Implement tests for palindrome function

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 const test = require('tape');
 const factorial = require('../javascript/algorithms/recursion/factorial');
+const palindrome = require('../javascript/algorithms/recursion/palindrome');
 const insertion = require('../javascript/algorithms/sorting/insertionSort');
 const selection = require('../javascript/algorithms/sorting/selectionSort');
 
@@ -37,6 +38,38 @@ test('Factorial should return 120 when 5 is passed in', (assert) => {
   const expected = 120;
 
   assert.equal(actual, expected, 'Factorial should return 120');
+  assert.end();
+});
+
+test('Palindrome should return true when empty string is passed in', (assert) => {
+  const actual = palindrome.palindrome('');
+  const expected = true;
+
+  assert.equal(actual, expected, 'Palindrome should return true');
+  assert.end();
+});
+
+test('Palindrome should return true when any string of length 1 is passed in', (assert) => {
+  const actual = palindrome.palindrome('a');
+  const expected = true;
+
+  assert.equal(actual, expected, 'Palindrome should return true');
+  assert.end();
+});
+
+test('Palindrome should return true when palindrome is passed in', (assert) => {
+  const actual = palindrome.palindrome('racecar');
+  const expected = true;
+
+  assert.equal(actual, expected, 'Palindrome should return true');
+  assert.end();
+});
+
+test('Palindrome should return false when palindrome is passed in', (assert) => {
+  const actual = palindrome.palindrome('dog');
+  const expected = false;
+
+  assert.equal(actual, expected, 'Palindrome should return true');
   assert.end();
 });
 
